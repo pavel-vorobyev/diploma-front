9e98524b35606153f900835b6166fe87b21719c5
Pavel Vorobyev <pavelvorobev.work@gmail.com>
2021-06-27 23:49:23 +0300

    The parseToIso8601 method added to the DateUtils object

--- app/src/main/java/com/pavelvorobyev/diploma/util/date.kt
+++ app/src/main/java/com/pavelvorobyev/diploma/util/date.kt
@@ -4,15 +4,20 @@ import android.annotation.SuppressLint
 import android.text.format.DateFormat
 import com.pavelvorobyev.diploma.util.extensions.Empty
 import java.text.SimpleDateFormat
+import java.util.*
 
 object DateUtils {
     @SuppressLint("SimpleDateFormat")
     private val iso8601DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
     private const val visiDateFormat = "dd MMM HH:mm"
 
+    fun parseToIso8601(date: String): Date? {
+        return iso8601DateFormat.parse(date)
+    }
+
     fun formatVisitDate(date: String): String {
         return try {
-            val dateParsed = iso8601DateFormat.parse(date)
+            val dateParsed = parseToIso8601(date)
             DateFormat.format(visiDateFormat, dateParsed).toString()
         } catch (e: Exception) {
             e.printStackTrace()
